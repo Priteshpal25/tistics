1b309e2167710cd81e74752cf250e432416bd38d
Priteshpal25 <priteshpal.prp@gmail.com>
2018-10-14 14:23:39 +0530

    created test files to maintain folder structure

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-'use strict';
+1'use strict';
 
 const fs = require('fs');
 const path = require('path');
--- models/schema/user.schema.js
+++ models/schema/user.schema.js
@@ -2,7 +2,45 @@ const Sequelize = require('sequelize');
 
 var userSchema={
  user_id:{
+ 	primaryKey:true,
  	allowNull:false,
  	type:Sequelize.INTEGER,
+ },
+ contact_id:{
+ 	allowNull:false,
+ 	type:Sequelize.INTEGER,
+ 	 references: {
+     // This is a reference to another model
+     model: Contacts,
+
+     // This is the column name of the referenced model
+     key: 'contacts_id',
+	}
+ },
+ attachment_id:{
+ 	allowNull:false,
+ 	type:Sequelize.INTEGER,
+ 	references{
+ 		model: Attachment,
+ 		key:'attachment_id',
+ 	}
+
+ },
+ route_id:{
+ 	type:Sequelize.INTEGER,
+ 	allowNull:false,
+ 	references:{
+ 		model: Route,
+ 		key: 'route_id'
+ 	}
+ },
+ login_id:{
+ 	type:Sequelize.INTEGER,
+ 	allowNull:false,
+ 	references:{
+ 		model:Login,
+ 		key:'login_id'
+ 	}
  }
+
 }
